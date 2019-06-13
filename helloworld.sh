eb493df8ffad18ccbdc7f5c6f1ca765eb0282b5f
Francesco Docchi <45453358+frankie-mceyes@users.noreply.github.com>
2019-06-13 10:14:30 +0200

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -6,13 +6,13 @@ COUNT=0
 
 for i in $(seq 1 11)
 do
-	while [[ "$LETT" != "${WORLD[$COUNT]}" ]]
-	do
-		echo -ne '\r'
-		LETT=$(head -c 1 /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*_+-')
-		echo -n "$WORD$LETT"
-	done
-	WORD="$WORD$LETT"
-	COUNT=$((COUNT+1))
+        while [[ "$LETT" != "${WORLD[$COUNT]}" ]]
+        do
+                echo -ne '\r'
+                LETT=$(head -c 1 /dev/urandom | tr -dc '0-9a-zA-Z!@#?àòè+ù§*éç°ç$%\/()=^&*_+-][')
+                echo -n "$WORD$LETT"
+        done
+        WORD="$WORD$LETT"
+        COUNT=$((COUNT+1))
 done
 echo
